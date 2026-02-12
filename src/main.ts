3a2a1be6b4d690f7a7832f914feb35090c571769
Yrbre <bariqfajar60@gmail.com>
2026-02-12 14:34:43 +0700

    first commit

---
+++ src/main.ts
@@ -0,0 +1,61 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config';
+import Aura from '@primeuix/themes/aura';
+import { definePreset } from '@primeuix/themes';
+
+import './assets/main.css'
+
+import App from './App.vue'
+import router from './router'
+import { colorScheme } from '@primeuix/themes/aura/autocomplete';
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+const FlashPreset = definePreset(Aura,
+{
+semantic:{
+    primary:{
+        50: '{blue-50}',
+        100: '{blue-100}',
+        200: '{blue-200}',
+        300: '{blue-300}',
+        400: '{blue-400}',
+        500: '{blue-500}',
+        600: '{blue-600}',
+        700: '{blue-700}',
+        800: '{blue-800}',
+        900: '{blue-900}',
+        950: '{blue-950}',
+    },
+    colorScheme:{
+        light :{
+            surface:{
+                0 : '#ffffff',
+                50: '{zinc-50}',
+                100: '{zinc-100}',
+                200: '{zinc-200}',
+                300: '{zinc-300}',
+                400: '{zinc-400}',
+                500: '{zinc-500}',
+                600: '{zinc-600}',
+                700: '{zinc-700}',
+                800: '{zinc-800}',
+                900: '{zinc-900}',
+                950: '{zinc-950}',
+                1000: '#101922',
+                }
+            }
+        }
+    }
+})
+app.use(PrimeVue,{
+    theme:{
+        preset : FlashPreset
+    }
+});
+
+app.mount('#app')
---
+++ src/page/Dashboard.vue
@@ -0,0 +1,7 @@
+<script setup>
+
+</script>
+
+<template>
+    <h1 class="text-black">Dashboard</h1>
+</template>
